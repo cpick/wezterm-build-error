5277dbaec2f9214481da945e36bf353ae453aa2c
Chris Pick <chris@chrispick.com>
2024-01-23 11:28:07 -0500

    Minimum demonstration of WezTerm Nix build failure

---
+++ README.md
@@ -0,0 +1,9 @@
+# Debug Nixpkgs WezTerm x86_64-darwin build failure
+
+https://github.com/NixOS/nixpkgs/issues/239384
+
+## Reproduce
+
+```sh
+nix build
+```
---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/4471857c0a4a8a0ffc7bdbeaf1b998746ce12a82";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-darwin;
+    in {
+
+      packages.x86_64-darwin.default = pkgs.darwin.apple_sdk_11_0.callPackage (
+        { rustPlatform, perl, System }:
+        rustPlatform.buildRustPackage {
+          name = "wezterm-build-error";
+          src = ./.;
+          cargoLock = { lockFile = ./Cargo.lock; };
+          nativeBuildInputs = [ perl ];
+          buildInputs = [ System ];
+        }
+      ) {
+        inherit (pkgs.darwin.apple_sdk_11_0.frameworks) System;
+      };
+
+    };
+}
